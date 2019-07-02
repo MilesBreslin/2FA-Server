98dd282cddba5b437e8b13bfa97ad2a99e7b372d
Miles Breslin <milesbreslin@gmail.com>
2019-07-02 15:24:57 -0700

    Simple websocket echo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"net/http"
+	"log"
+    "github.com/gorilla/websocket"
 )
 
 func main() {
+	http.HandleFunc("/websocket", handleWebsocket)
 	http.Handle("/", http.FileServer(http.Dir("./web/")))
 	http.ListenAndServe(":8000", nil)
+}
+
+func handleWebsocket(w http.ResponseWriter, r *http.Request) {
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) (bool) {
+			return true
+		},
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer ws.Close()
+
+	for {
+		messageType, msg, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		ws.WriteMessage(messageType, msg)
+	}
 }
