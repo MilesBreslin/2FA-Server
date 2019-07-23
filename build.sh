746f57dc6c9ae251aa1fd1d611c7e17bd8a9af38
Miles Breslin <milesbreslin@gmail.com>
2019-07-23 16:08:49 -0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir web_build 2>/dev/null
+
+for file in web/* ; do
+    build_utils/static_web.sh "$file" "web_build/${file/web\//}"
+done
+
+mkdir build 2>/dev/null
+go build -o build/server cmd/server/main.go
