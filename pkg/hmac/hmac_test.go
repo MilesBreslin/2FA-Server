c9f8deef692a3d0209790eaf74431a69fb3110fb
Miles Breslin <milesbreslin@gmail.com>
2019-07-25 15:21:18 -0700

    Implement HMAC unit test

---
+++ pkg/hmac/hmac_test.go
@@ -0,0 +1,12 @@
+package hmac
+
+import (
+    "testing"
+)
+
+func TestGetHOTPToken(t *testing.T) {
+    out := getHOTPToken("lhe4kfhfqapxipzmohswb6i5adg2gauh", 0)
+    if out != "449770" {
+        t.Errorf("getHOTPToken test token expecting 449770, got %s",out)
+    }
+}
