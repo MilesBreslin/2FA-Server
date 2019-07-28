0a7484ef20ad23278189e2f7af390a89acab0308
Miles Breslin <milesbreslin@gmail.com>
2019-07-27 23:03:42 -0700

    Ensure proper output

--- pkg/realtime_api/methods/keys.go
+++ pkg/realtime_api/methods/keys.go
@@ -11,32 +11,34 @@ func init() {
 }
 
 func addKey_method(in []interface{}) ([]interface{}, uint16) {
+    out := make([]interface{}, 0)
     for _, obj := range in {
         if secret, ok := obj.(map[string]interface{})["secret"]; ok {
             switch secret.(type) {
             case string:
-                keys.AddKey(secret.(string))
+                k := keys.AddKey(secret.(string))
+                out = append(out, k.Id)
             default:
-                return nil, 400
+                return out, 400
             }
         } else {
-            return nil, 400
+            return out, 400
         }
     }
-    return nil, 200
+    return out, 200
 }
 
 func getKey_method(in []interface{}) ([]interface{}, uint16) {
 	out := make([]interface{}, 0)
     for _, obj := range in {
-        if secret, ok := obj.(map[string]interface{})["id"]; ok {
-            switch secret.(type) {
+        if id, ok := obj.(map[string]interface{})["id"]; ok {
+            switch id.(type) {
             case float64:
-                k, err := keys.GetKey(id.(string))
+                k, err := keys.GetKey(uint64(id.(float64)))
                 if err != nil {
                 	return nil, 404
                 }
-                append(out, *k)
+                out = append(out, *k)
             default:
                 return nil, 400
             }
@@ -44,5 +46,5 @@ func getKey_method(in []interface{}) ([]interface{}, uint16) {
             return nil, 400
         }
     }
-    return nil, 200
+    return out, 200
 }
